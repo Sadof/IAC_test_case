4669e65530566d5a4e7c961246ff504961e17122
Sadof <sadof91195@gmail.com>
2023-10-20 10:24:07 +0300

    Added Role entity and Login controller
    Signed-off-by: Sadof <sadof91195@gmail.com>

---
+++ migrations/Version20231020065323.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231020065323 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE role_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE role (id INT NOT NULL, role_name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('ALTER TABLE "user" RENAME COLUMN rfio TO fio');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE role_id_seq CASCADE');
+        $this->addSql('DROP TABLE role');
+        $this->addSql('ALTER TABLE "user" RENAME COLUMN fio TO rfio');
+    }
+}
